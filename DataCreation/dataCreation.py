ad96f98d6a6b00c2b209a5ddb6a415b64a037703
gnoffl <gernot.schmitz@tu-dortmund.de>
2021-12-19 15:01:22 +0100

    divided file in classes for different distributions. implemented a "natural looking" data set and a stack of cubes

--- DataCreation/dataCreation.py
+++ DataCreation/dataCreation.py
@@ -1,102 +1,177 @@
 from typing import List, Tuple
+import visualization
 
 import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib as mpl
-import random
+from abc import ABC, abstractmethod
+import numpy as np
 
 
-#first entry of dims will be x-axis, second will be y-axis
-def visualize_2d(df: pd.DataFrame, dims: Tuple[str, str], class_column: str or None=None):
-    x_name, y_name = dims
-    plt.figure(0, figsize=(5.5, 4))
-    plt.clf()
-
-    x_min, x_max = df[x_name].min(), df[x_name].max()
-    y_min, y_max = df[y_name].min(), df[y_name].max()
-
-    x_pad = 0.1 * (x_max - x_min)
-    y_pad = 0.1 * (y_max - y_min)
-
-    x_axis_min = x_min - x_pad
-    x_axis_max = x_max + x_pad
-    y_axis_min = y_min - y_pad
-    y_axis_max = y_max + y_pad
-
-    plt.xlim(x_axis_min, x_axis_max)
-    plt.ylim(y_axis_min, y_axis_max)
-    plt.xlabel(x_name)
-    plt.ylabel(y_name)
-    if class_column:
-        plt.scatter(df[x_name], df[y_name], c=df[class_column], cmap=plt.cm.Set1, edgecolor="k")
-    else:
-        plt.scatter(df[x_name], df[y_name], cmap=plt.cm.Set1, edgecolor="k")
-    plt.show()
+"""
+3 – 5 Classes
+5 – 20 Variables with Information
+~5 random Variables
+Create Dependencies that require Binning
+1.000 – 10.000 Datenpunkte
+MVTNorm oä Pakete für synthetische Datensätze
+"""
 
 
-def create_3d_plot(df: pd.DataFrame, dims: Tuple[str, str, str], azim: int = 110, elev: int = -150, class_column: str or None = None):
-    x_name, y_name, z_name = dims
-    fig = plt.figure(1, figsize=(8, 6))
-    plt.clf()
-    ax = Axes3D(fig, elev=elev, azim=azim, auto_add_to_figure=False)
-    fig.add_axes(ax)
-    if class_column:
-        ax.scatter(
-            df[x_name],
-            df[y_name],
-            df[z_name],
-            c=df[class_column],
-            cmap=plt.cm.Set1,
-            edgecolor="k",
-            s=40,
-        )
-    else:
-        ax.scatter(
-            df[x_name],
-            df[y_name],
-            df[z_name],
-            cmap=plt.cm.Set1,
-            edgecolor="k",
-            s=40,
-        )
-    ax.w_xaxis.set_ticklabels([])
-    ax.w_yaxis.set_ticklabels([])
-    ax.w_zaxis.set_ticklabels([])
-    ax.set_xlabel(x_name)
-    ax.set_ylabel(y_name)
-    ax.set_zlabel(z_name)
-    plt.show()
+def add_gaussian_noise(data: pd.Series, sd: int):
+    length = len(data)
+    noise = np.random.normal(0, sd, length)
+    return data + noise
 
 
-def visualize_3d(df: pd.DataFrame, dims: Tuple[str, str, str], azim: int or None = None, elev: int = -150,  class_column: str or None=None):
-    if isinstance(azim, int):
-        create_3d_plot(df, dims, azim, elev, class_column)
-    else:
-        azim = 110
-        create_3d_plot(df, dims, azim, elev, class_column)
-        create_3d_plot(df, dims, azim - 90, elev, class_column)
+def add_random_dims(data: pd.DataFrame, dim_names: List[str]):
+    length = len(data)
+    for name in dim_names:
+        range_ = np.random.uniform(-1, 1, 2)*1000
+        data[name] = [val for val in np.random.uniform(range_[0], range_[1], length)]
 
 
-def main():
-    df = pd.DataFrame()
-    classes = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
-    dim_01 = [1, 2, 3, 4, 5, 12, 13, 15, 12, 11]
-    dim_02 = [5, 6, 7, 9, 4, .1, .4, .5, .6, .2]
-    dim_03 = [3, 5, 8, 1, 4, 7, 9, 5, 3, 2]
-    df["classes"] = classes
-    df["dim_01"] = dim_01
-    df["dim_02"] = dim_02
-    df["dim_03"] = dim_03
-    #visualize_2d(df, ("dim_01", "dim_02"))
-    #visualize_2d(df, ("dim_01", "dim_03"), "classes")
-    #visualize_3d(df, ("dim_01", "dim_02", "dim_03"))
-    for i in range(36):
-        visualize_3d(df, ("dim_01", "dim_02", "dim_03"), class_column="classes", azim=10*i, elev=-150)
+class Data(ABC):
+    data: pd.DataFrame
+    members: List[int]
 
 
+class NaturalData(Data):
+    def __init__(self, members: List[int]):
+        np.random.seed(42)
+        if len(members) != 5:
+            raise Exception("members needs to be initialized with a List of length 5!")
+        self.members = members
+        class_0 = self.create_class_0()
+        class_1 = self.create_class_1()
+        class_2 = self.create_class_2()
+        class_3 = self.create_class_3()
+        class_4 = self.create_class_4()
+        self.data = pd.concat([class_0, class_1, class_2, class_3, class_4])
+        add_random_dims(self.data, ["rand_00", "rand_01", "rand_02"])
+
+    def create_class_0(self):
+        new_df = pd.DataFrame()
+        new_df["classes"] = [0 for _ in range(self.members[0])]
+
+        dim_00 = np.random.normal(-47, 10, (self.members[0],))
+        new_df["dim_00"] = dim_00
+
+        dim_01 = np.random.normal(30, 10, (self.members[0],))
+        new_df["dim_01"] = dim_01
+
+        dim_02 = np.random.normal(0, 20, (self.members[0],))
+        new_df["dim_02"] = dim_02
+        return new_df
+
+    def create_class_1(self):
+        new_df = pd.DataFrame()
+        new_df["classes"] = [1 for _ in range(self.members[1])]
+
+        dim_00 = np.random.normal(140, 50, (self.members[1],))
+        new_df["dim_00"] = dim_00
+
+        dim_01 = np.random.normal(80, 50, (self.members[1],))
+        new_df["dim_01"] = dim_01
+
+        dim_02 = np.random.normal(50, 20, (self.members[1],))
+        new_df["dim_02"] = dim_02
+
+        return new_df
+
+    def create_class_2(self):
+        new_df = pd.DataFrame()
+        new_df["classes"] = [2 for _ in range(self.members[2])]
+
+        dim_00 = np.random.normal(-50, 20, (self.members[2],))
+        new_df["dim_00"] = dim_00
+
+        dim_01 = np.random.normal(150, 40, (self.members[2],))
+        new_df["dim_01"] = dim_01
+
+        dim_02 = np.random.normal(0, 20, (self.members[2],))
+        new_df["dim_02"] = dim_02
+        return new_df
+
+    def create_class_3(self):
+        new_df = pd.DataFrame()
+        new_df["classes"] = [3 for _ in range(self.members[3])]
+
+        dim_00 = np.random.normal(20, 15, (self.members[3],))
+        new_df["dim_00"] = dim_00
+
+        dim_01 = np.random.normal(80, 25, (self.members[3],))
+        new_df["dim_01"] = dim_01
+
+        dim_02 = np.random.normal(30, 20, (self.members[3],))
+        new_df["dim_02"] = dim_02
+        return new_df
+
+    def create_class_4(self):
+        new_df = pd.DataFrame()
+        new_df["classes"] = [4 for _ in range(self.members[4])]
+
+        dim_01 = np.random.normal(0, 25, (self.members[4],))
+        new_df["dim_01"] = dim_01
+
+        dim_00 = np.random.normal(30, 20, (self.members[4],))
+        new_df["dim_00"] = dim_00
+
+        dim_02 = np.random.normal(30, 20, (self.members[4],))
+        new_df["dim_02"] = dim_02
+
+        return new_df
+
+
+class GeometricUniformData(Data):
+    def __init__(self, members: List[int]):
+        np.random.seed(42)
+        self.members = members
+        self.create_data()
+        add_random_dims(self.data, ["rand_00", "rand_01", "rand_02"])
+
+    #todo: different density of data in overlap of classes, probably needs to be removed -.-
+    @staticmethod
+    def get_low_values(length: int):
+        return np.random.uniform(-0.1, 1.1, length)
+
+    @staticmethod
+    def get_high_values(length: int):
+        return np.random.uniform(0.9, 2.1, length)
+
+    def create_class_data(self, dist: Tuple[int, int, int], class_number: int):
+        df = pd.DataFrame()
+        x_high, y_high, z_high = dist
+        try:
+            members = self.members[class_number]
+        except IndexError:
+            members = 0
+
+        df["classes"] = [class_number for _ in range(members)]
+        df["dim_00"] = self.get_high_values(members) if x_high else self.get_low_values(members)
+        df["dim_01"] = self.get_high_values(members) if y_high else self.get_low_values(members)
+        df["dim_02"] = self.get_high_values(members) if z_high else self.get_low_values(members)
+        return df
+
+    def create_data(self):
+        class_list = []
+        class_number = 0
+        for x in [0, 1]:
+            for y in [0, 1]:
+                for z in [0, 1]:
+                    class_list.append(self.create_class_data((x, y, z), class_number))
+                    class_number += 1
+        self.data = pd.concat(class_list)
 
 
 if __name__ == "__main__":
-    main()
+    members = [200 for _ in range(5)]
+    df = GeometricUniformData(members).data
+    visualization.visualize_2d(df, ("dim_00", "dim_01"), class_column="classes")
+    visualization.visualize_2d(df, ("dim_01", "dim_02"), class_column="classes")
+    visualization.visualize_2d(df, ("dim_02", "dim_00"), class_column="classes")
+    #visualization.visualize_3d(df, ("dim_00", "dim_01", "rand3"), class_column="classes")
+    #visualization.create_3d_gif(df=df, dims=("dim_00", "dim_01", "dim_02"), name="cubes_6_classes", class_column="classes", steps=72, duration=50)
+
 
