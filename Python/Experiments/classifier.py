c2fb52fe6e3ba362869eb0aa9eed68eb6bd45494
gnoffl <gernot.schmitz@tu-dortmund.de>
2022-02-02 17:30:54 +0100

    prediction now requires a column name for the predictions dataset will be saved using the data set methods. standard notes will be made, but can be expanded

--- Python/Experiments/classifier.py
+++ Python/Experiments/classifier.py
@@ -26,34 +26,46 @@ def train_decision_tree(df: pd.DataFrame, max_depth=5, min_samples_leaf=5) -> tr
     return decision_tree
 
 
-def predict_classes(trained_tree: tree.DecisionTreeClassifier, data: pd.DataFrame) -> None:
+def predict_classes(trained_tree: tree.DecisionTreeClassifier, data: pd.DataFrame, pred_col_name: str) -> None:
     """
     uses a trained decision tree to predict the classes of the given data
     :param trained_tree: the trained decision tree
     :param data: the data
+    :param pred_col_name: name for the column, in which the predicted classes will be stored
     """
+    if pred_col_name in data.columns:
+        raise dc.CustomError("Column already exists!")
     values = data.drop(columns=["classes"])
     results = trained_tree.predict(values)
-    data["predicted_classes"] = results
+    data[pred_col_name] = results
 
 
-#todo: create then use saving functionality of data class
-def save_predicted_data(members: List[int], depth: int = 5, min_samples_leaf: int = 5) -> Tuple[pd.DataFrame, tree.DecisionTreeClassifier]:
+def save_predicted_data(dataset: dc.MaybeActualDataSet,
+                        pred_col_name: str,
+                        depth: int = 5,
+                        min_samples_leaf: int = 5,
+                        notes: str = "") -> Tuple[pd.DataFrame, tree.DecisionTreeClassifier]:
     """
-    creates data from a MaybeActualDataSet, trains a decision tree on it, uses the tree to predict the classes and saves
-    the resulting dataframe to a csv under Bachelor/Python/Experiments/Data
-    :param members: list with the number of members for each class in the data set
+    trains a decision tree on a MaybeActualDataSet, uses the tree to predict the classes and saves
+    the resulting dataset
+    :param dataset: the data on which predictions will be made
+    :param pred_col_name: name for the column, in which the predicted classes will be stored
     :param depth: maximal depth of the decision tree
     :param min_samples_leaf: minimum number of data points per leaf in the decision tree
+    :param notes: Notes to be saved in the description of the data class object
     :return: Tuple with the dataframe and the trained tree
     """
-    df = dc.MaybeActualDataSet(members).data
+    df = dataset.data
     trained_tree = train_decision_tree(df, max_depth=depth, min_samples_leaf=min_samples_leaf)
-    predict_classes(trained_tree, df)
-    df.to_csv(os.path.join(os.path.dirname(__file__), "Data", f"{members[0]}members_{depth}depth_{min_samples_leaf}min_samples.csv"))
+    predict_classes(trained_tree=trained_tree, data=df, pred_col_name=pred_col_name)
+
+    dataset.extend_notes(f"Predicted classes using Decision Tree in column \"{pred_col_name}\".")
+    dataset.extend_notes(f"Parameters: max_depth={depth}, min_samples_leaf={min_samples_leaf}")
+    dataset.save(notes=notes)
     return df, trained_tree
 
 
+#todo: make this work from saved data
 def visualize(df: pd.DataFrame, trained_tree: tree.DecisionTreeClassifier) -> None:
     """
     visualizes data (original vs predicted) as multiple 2d pictures as well as the tree as a diagram
@@ -79,22 +91,12 @@ def visualize(df: pd.DataFrame, trained_tree: tree.DecisionTreeClassifier) -> No
     graph.view()
 
 
-#todo: create then use saving functionality of data class
-def load_df(path: str) -> pd.DataFrame:
-    """
-    loads dataframe from csv file
-    :param path: path to the csv
-    :return: the loaded dataframe
-    """
-    return pd.read_csv(path, index_col=0)
-
-
 def run() -> None:
     """
     runs the training process and visualizes results
     """
     members_ = [1000 for _ in range(6)]
-    data, trained_tree = save_predicted_data(members=members_)
+    data, trained_tree = save_predicted_data(members=members_, pred_col_name="predicted_classes")
     #data = load_df("1000members_5depth_5min_samples.csv")
     #visualize(data, trained_tree)
     matrix = vs.get_change_matrix(data, ("classes", "predicted_classes"))
@@ -102,5 +104,9 @@ def run() -> None:
 
 
 if __name__ == "__main__":
-    run()
-
+    #run()
+    #todo: run_hics darf nicht die Splate "predicted_classes" sehen
+    """members_ = [1000 for _ in range(6)]
+    set_ = dc.MaybeActualDataSet(members_)
+    set_.run_hics()
+    data, trained_tree_ = save_predicted_data(dataset=set_, pred_col_name="predicted_classes")"""
