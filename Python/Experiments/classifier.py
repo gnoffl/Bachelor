c9be308bd65b249f4ddb27f1ae1c38c91f75c684
gnoffl <gernot.schmitz@tu-dortmund.de>
2022-02-05 17:52:56 +0100

    most functions now take data objects instead of dataFrames. This was done to use the new feature of the attribute "data_columns" from Data. Avoids using irrelevant columns (like class columns) when predicting classes or training trees. DecisionTrees are now saved, and only trained, if no Tree is available for the dataset thats used.

--- Python/Experiments/classifier.py
+++ Python/Experiments/classifier.py
@@ -5,37 +5,46 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import graphviz
 import os
+import pickle
 
 import Python.DataCreation.dataCreation as dc
 import Python.DataCreation.visualization as vs
 
 
 
-def train_decision_tree(df: pd.DataFrame, max_depth=5, min_samples_leaf=5) -> tree.DecisionTreeClassifier:
+def train_decision_tree(dataset: dc.Data, max_depth=5, min_samples_leaf=5) -> tree.DecisionTreeClassifier:
     """
     trains a decision tree for the given data
-    :param df: data
+    :param dataset: data
     :param max_depth: max depth of the decision tree
     :param min_samples_leaf: minimum samples per leaf in the decision tree
     :return: the trained tree
     """
-    classes = df["classes"]
-    values = df.drop(columns=["classes"])
-    decision_tree = tree.DecisionTreeClassifier(max_depth=max_depth, min_samples_leaf=min_samples_leaf)
-    decision_tree.fit(values, classes)
+    df = dataset.data
+    tree_path = os.path.join(dataset.path, "tree_classifier.pkl")
+    if not os.path.isfile(tree_path):
+        classes = df["classes"]
+        values = df[dataset.data_columns]
+        decision_tree = tree.DecisionTreeClassifier(max_depth=max_depth, min_samples_leaf=min_samples_leaf)
+        decision_tree.fit(values, classes)
+    else:
+        with open(tree_path, "rb") as f:
+            print("loading tree!")
+            decision_tree = pickle.load(f)
     return decision_tree
 
 
-def predict_classes(trained_tree: tree.DecisionTreeClassifier, data: pd.DataFrame, pred_col_name: str) -> None:
+def predict_classes(trained_tree: tree.DecisionTreeClassifier, dataset: dc.Data, pred_col_name: str) -> None:
     """
     uses a trained decision tree to predict the classes of the given data
     :param trained_tree: the trained decision tree
-    :param data: the data
+    :param dataset: the data
     :param pred_col_name: name for the column, in which the predicted classes will be stored
     """
+    data = dataset.data
     if pred_col_name in data.columns:
         raise dc.CustomError("Column already exists!")
-    values = data.drop(columns=["classes"])
+    values = data[dataset.data_columns]
     results = trained_tree.predict(values)
     data[pred_col_name] = results
 
@@ -44,25 +53,30 @@ def save_predicted_data(dataset: dc.MaybeActualDataSet,
                         pred_col_name: str,
                         depth: int = 5,
                         min_samples_leaf: int = 5,
-                        notes: str = "") -> Tuple[pd.DataFrame, tree.DecisionTreeClassifier]:
+                        notes: str = "") -> tree.DecisionTreeClassifier:
     """
     trains a decision tree on a MaybeActualDataSet, uses the tree to predict the classes and saves
-    the resulting dataset
+    the resulting dataset. Also saves the resulting tree at the path of the dataset.
     :param dataset: the data on which predictions will be made
     :param pred_col_name: name for the column, in which the predicted classes will be stored
     :param depth: maximal depth of the decision tree
     :param min_samples_leaf: minimum number of data points per leaf in the decision tree
     :param notes: Notes to be saved in the description of the data class object
-    :return: Tuple with the dataframe and the trained tree
+    :return: Trained decision tree
     """
     df = dataset.data
-    trained_tree = train_decision_tree(df, max_depth=depth, min_samples_leaf=min_samples_leaf)
-    predict_classes(trained_tree=trained_tree, data=df, pred_col_name=pred_col_name)
+    trained_tree = train_decision_tree(dataset, max_depth=depth, min_samples_leaf=min_samples_leaf)
+    predict_classes(trained_tree=trained_tree, dataset=dataset, pred_col_name=pred_col_name)
 
-    dataset.extend_notes(f"Predicted classes using Decision Tree in column \"{pred_col_name}\".")
-    dataset.extend_notes(f"Parameters: max_depth={depth}, min_samples_leaf={min_samples_leaf}")
+    dataset.extend_notes_by_one_line(f"Predicted classes using Decision Tree in column \"{pred_col_name}\".")
+    dataset.extend_notes_by_one_line(f"Parameters: max_depth={depth}, min_samples_leaf={min_samples_leaf}")
+    dataset.end_paragraph_in_notes()
     dataset.save(notes=notes)
-    return df, trained_tree
+    tree_path = os.path.join(dataset.path, "tree_classifier.pkl")
+    if not os.path.isfile(tree_path):
+        with open(tree_path, "wb") as f:
+            pickle.dump(trained_tree, f)
+    return trained_tree
 
 
 #todo: make this work from saved data
@@ -95,16 +109,19 @@ def run() -> None:
     """
     runs the training process and visualizes results
     """
-    members_ = [1000 for _ in range(6)]
-    data, trained_tree = save_predicted_data(members=members_, pred_col_name="predicted_classes")
-    #data = load_df("1000members_5depth_5min_samples.csv")
+    #members_ = [1000 for _ in range(6)]
+    #dataset = dc.MaybeActualDataSet(members_)
+    #trained_tree = save_predicted_data(dataset=dataset, pred_col_name="predicted_classes")
+    dataset = dc.MaybeActualDataSet.load("D:\\Gernot\\Programmieren\\Bachelor\\Python\\Experiments\\Data\\220205_173440_MaybeActualDataSet")
+    dataset.run_hics()
+    trained_tree = save_predicted_data(dataset=dataset, pred_col_name="predicted_classes_1")
     #visualize(data, trained_tree)
-    matrix = vs.get_change_matrix(data, ("classes", "predicted_classes"))
-    print(matrix)
+    #matrix = vs.get_change_matrix(data, ("classes", "predicted_classes"))
+    #print(matrix)
 
 
 if __name__ == "__main__":
-    #run()
+    run()
     #todo: run_hics darf nicht die Splate "predicted_classes" sehen
     """members_ = [1000 for _ in range(6)]
     set_ = dc.MaybeActualDataSet(members_)
